992ee815099c22707ad2bf7c21fc7455c2cfebef
amberlauer <amber.lauer@tunl.duke.edu>
2020-07-08 19:32:32 -0400

    test version with only 5 runs

--- slurm_submit_1b.sh
+++ slurm_submit_1b.sh
@@ -2,32 +2,30 @@
 
 echo "what is the round number?"
 read number
-echo "what is the start number"
-read numero
 echo "what's the multiplier
 
 1. 100
 2. .01
 "
 read mult
-
+let "numero=${numer}-1"
 if [ "${mult}" = "1" ] ; then
         factor=100
-        cp slurm_scavenger_10108_1b_template.q slurm_scavenger_10108_${number}_${factor}.q
-        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+        cp slurm_scavenger_10108_1b_template2.q slurm_scavenger_10108_1b_${number}_${factor}.q
+        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_1b_${number}_${factor}.q
 
 elif [ "${mult}" = "2" ] ; then
         factor=.01
-        cp slurm_scavenger_10108_1b_template.q slurm_scavenger_10108_${number}_${factor}.q
-        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+        cp slurm_scavenger_10108_1b_template2.q slurm_scavenger_10108_1b_${number}_${factor}.q
+        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_1b_${number}_${factor}.q
 fi
 
-sed -i 's|number|'${number}'|g'  slurm_scavenger_10108_${number}_${factor}.q
-sed -i 's|numero|'${numero}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+sed -i 's|number|'${number}'|g'  slurm_scavenger_10108_1b_${number}_${factor}.q
+sed -i 's|numero|'${numero}'|g'  slurm_scavenger_10108_1b_${number}_${factor}.q
 
-mkdir /work/al363/runs/runs_x${number}_${factor}
-#sbatch slurm_scavenger_10108_${number}_${factor}.q >> job_number_${number}_${factor}.txt
+#sbatch slurm_scavenger_10108_1b_${number}_${factor}.q >> job_number_${number}_${factor}.txt
 
+#### old junk that didn't work
 #job_number=$(tail -c 9 job_number.txt)
 
 #cp move_data_template.sh move_data_${number}.sh
