f8b39adf2c4b42f73412414f3ab6ced4ce060f4a
amberlauer <amber.lauer@tunl.duke.edu>
2021-03-24 17:49:22 -0400

    still trying to get low overhead ready

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -3,12 +3,31 @@
 echo "what is the round number?"
 read number
 let "numero=${number}-1"
-cp slurm_scavenger_10108_template.q slurm_scavenger_10108_${number}_01.q
-sed -i 's|number|'${number}'|g'  slurm_scavenger_10108_${number}_01.q
-sed -i 's|numero|'${numero}'|g'  slurm_scavenger_10108_${number}_01.q
 
-mkdir /work/al363/runs/low_overhead/runs_x.01_${number}
-sbatch slurm_scavenger_10108_${number}_01.q >> job_number_${number}_01.txt
+echo "what's the multiplier
+
+1. 100
+2. .01
+"
+read mult
+let "numero=${number}-1"
+if [ "${mult}" = "1" ] ; then
+        factor=100
+        cp slurm_scavenger_10108_template.q slurm_scavenger_10108_${number}_${factor}.q
+        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+
+elif [ "${mult}" = "2" ] ; then
+        factor=.01
+        cp slurm_scavenger_10108_template.q slurm_scavenger_10108_${number}_${factor}.q
+        sed -i 's|factor|'${factor}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+fi
+
+
+sed -i 's|number|'${number}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+sed -i 's|numero|'${numero}'|g'  slurm_scavenger_10108_${number}_${factor}.q
+
+mkdir /work/al363/runs/low_overhead/runs_x${factor}_${number}
+sbatch slurm_scavenger_10108_${number}_${factor}.q >> job_number_${number}_01.txt
 
 #job_number=$(tail -c 9 job_number.txt)
 
