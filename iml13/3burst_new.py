a34896ff22587fc313e8f8ad227d4b858165e90e
amberlauer <amber.lauer@tunl.duke.edu>
2021-03-31 19:43:53 -0400

    macro

--- iml13/3burst_new.py
+++ iml13/3burst_new.py
@@ -54,7 +54,7 @@ for i in range(0, max, 1): # goes through odd files
     path2 = '/LOGS/history.data'
     file_path =runs_folder+ path1 + path2
     print(file_path)
-    #file_path=str(file_path)
+    file_path=str(file_path)
     with open( file_path, 'r') as f:
 
         info_starts = 0
