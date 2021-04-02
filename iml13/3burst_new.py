25ae070fb24c084e188a38794bb4274d3ad6a299
amberlauer <amber.lauer@tunl.duke.edu>
2021-04-02 19:46:42 -0400

    macros

--- iml13/3burst_new.py
+++ iml13/3burst_new.py
@@ -47,6 +47,7 @@ with open(file_name) as f:
 #lf=pd.DataFrame({'col':[os.path.splitext(x)[0] for x in file_list]})
 #index=lf.index0
 max=int(len(lines))
+max=max(max,100)
 s = 'baseline'
 #goes through list
 i=0
