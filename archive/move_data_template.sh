bb428f5d08546d529e451b5f1799931d10fb9fec
acoles <acoles@bnl.gov>
2021-07-20 12:21:58 -0400

    message

--- archive/move_data_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -r runs_x100_number /datacommons/phy-champagne-lauer/
-mkdir /datacommons/phy-champagne-lauer/runs_x100_number/errors
-mv /work/al363/sens/errors/slurm._job_number* /datacommons/phy-champagne-lauer/runs_x100_number/errors/
